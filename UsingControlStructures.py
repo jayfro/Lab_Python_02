fda53b221140678b9e10424dd5590a0091d84c8f
jayfro <lilclinx@yahoo.co.nz>
2012-06-20 19:27:58 +0000

    {{Day2}}

---
+++ UsingControlStructures.py
@@ -0,0 +1,61 @@
+theInput = raw_input("Enter an integer: ")
+
+
+i=theInput
+i= int (i)
+
+if i % 2 == 0:
+   
+ print "even"
+
+if i % 2 == 1:
+
+ print "odd"
+
+
+print "-----------------------------------------"
+primarySchoolAge = 5
+legalVotingAge = 18
+presidentAge = 45
+retirementAge = 60
+
+personsAge=input("Enter an age: ")
+
+
+if personsAge<primarySchoolAge:
+    print "too young"
+
+elif personsAge>=presidentAge:
+    print "Vote for me"
+
+elif personsAge>=legalVotingAge:
+    print "Remember to vote"
+
+elif personsAge<presidentAge:
+    print "You can't be president"
+
+elif persosAge>=retirementAge:
+    print "Too Old"
+
+
+print "-------------------------------------------------"
+
+for i in range(40,0,-1):
+    if i%3 == 0:
+         print i
+        
+print "------------------------------------------------------------"
+
+x = 1
+
+ while (x <= 100)
+ 
+if ((x % 7) == 0)
+
+   print x
+
+   x+=1
+
+
+   
+
